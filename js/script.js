cdafbdaf9c8863898ab7c7fb1b5d00ddcbd80f27
DanielNahshonov <dnakhshunov@gmail.com>
2024-06-02 20:07:13 +0300

    script new

--- js/script.js
+++ js/script.js
@@ -10,32 +10,51 @@ var twoSum = function (nums, target) {
   return count;
 };
 
-
-
 let num = [1, 2, 3, 4, 5, 1];
 let target = 6;
 
 // console.log(twoSum(num, target));
 
-
-function twoSum2(num,target){
-    let count=0
-    let sum=0
-    for(let i=0;i<num.length;i++){
-        sum=num[i]+num[i+1]
-        console.log('sum is '+sum)
-        if(sum<target){
-            sum+=num[i+2]
-            console.log(sum)
-        }else if(sum== target){
-            count+=1
-            console.log(count)
-        }else if(sum>target){
-            sum=0
-        }
+function twoSum2(num, target) {
+  let count = 0;
+  let sum = 0;
+  for (let i = 0; i < num.length; i++) {
+    console.log("i is " + i);
+    sum = num[i] + num[i + 1];
+    console.log("sum is " + sum);
+    if (sum < target) {
+      sum += num[i + 2];
+      console.log(sum);
+    } else if (sum == target) {
+      count += 1;
+      console.log(count);
+    } else if (sum > target) {
+      sum = 0;
     }
-    
-    return count
+  }
+
+  return count;
 }
 
-console.log(twoSum2(num,target))
+console.log(twoSum2(num, target));
+
+function newOne(num, target) {
+  let count = 0;
+  let sum = 0;
+  for (let i = 0; i < num.length; i++) {
+    console.log("i is " + i);
+    sum += num[i] 
+    console.log("sum is " + sum);
+    if (sum < target) {
+      sum += num[i + 2];
+      console.log(sum);
+    } else if (sum == target) {
+      count += 1;
+      console.log(count);
+    } else if (sum > target) {
+      sum = 0;
+    }
+  }
+
+  return count;
+}

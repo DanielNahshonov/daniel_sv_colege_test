3ce75f90745d329201076542a761dc27203c0d2b
DanielNahshonov <dnakhshunov@gmail.com>
2024-06-02 16:54:43 +0300

    1

---
+++ react/my-project/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Routes, Route} from 'react-router-dom'
+
+import {useNavigate} from 'react-router-dom’
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path="/" element={} />
+    </Routes>
+  );
+}
+
+export default App;
